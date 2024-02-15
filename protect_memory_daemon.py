7f82cc54d8d626720b0e7c3618c5ebf7df7a5c58
Luiz Trindade <149267777+Luiz-Trindade@users.noreply.github.com>
2024-02-15 14:31:48 -0300

    Add files via upload

--- protect_memory_daemon.py
+++ protect_memory_daemon.py
@@ -4,19 +4,17 @@
 
 import psutil
 from time import sleep
-import daemon
+from daemon import DaemonContext
+from threading import Thread
+from os import getpgid
+from random import randint
 
 percent_to_use = 0.8
-
-# Permitido usar até certa porcentagem da memória ram
 memory_to_use = int(psutil.virtual_memory().total * percent_to_use)
 
 def VerifyEnergyProfile():
     energy = psutil.sensors_battery().power_plugged
-    if energy:
-        sleep(0.5)
-    else:
-        sleep(1)
+    sleep(0.5 if energy else randint(1, 2))
 
 def VerifyMemory():
     all_processes = psutil.process_iter(attrs=["pid"])
@@ -26,10 +24,13 @@ def VerifyMemory():
         process_pid = psutil.Process(pid)
         memory = int(process_pid.memory_info().rss)
 
-        if memory >= memory_to_use:
-            process_pid.terminate()
+        if memory >= memory_to_use and pid != getpid():
+            try:
+                process_pid.terminate()
+            except: pass
 
 def Main():
+    with DaemonContext():
         try:
             while True:
                 VerifyMemory()
@@ -37,6 +38,15 @@ def Main():
         except KeyboardInterrupt:
             exit()
             
+'''def RunDaemon():
+    with DaemonContext():
+        Main()'''
+
 if __name__ == "__main__":
-    with daemon.DaemonContext():
-        Main()
+    main_thread = Thread(target=Main)
+
+    try:
+        main_thread.start()        
+        main_thread.join()
+    except:
+        pass
